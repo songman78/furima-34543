614efa70a5234f47a708da043260c6427ccc63e6
songman78 <song.matsui@gmail.com>
2021-02-25 21:11:31 +0900

    ログイン画面遷移、新規登録画面遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # validates :nickname, presence: true
-  # validates :encrypted_password, presence: true
-  # # , length: { minimum: 6}, format: { with: /\A[a-z0-9]+\z/i}
-  # validates :last_name, presence: true
-  # validates :first_name, presence: true
-  # validates :last_name_kana, presence: true
-  # validates :first_name_kana, presence: true
-  # validates :birth_date,presence:true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date,presence:true
 end
