a58b9d7684a4836c5bf05973aab699b7c163281c
songman78 <song.matsui@gmail.com>
2021-02-25 17:36:23 +0900

    データを保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname,:email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+      :sign_up, keys: [:nickname,:email,:encrypted_password, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
     )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :password, presence: true
-  , length: { minimum: 6}, format: { with: /\A[a-z0-9]+\z/i}
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date,presence:true
+  # validates :nickname, presence: true
+  # validates :encrypted_password, presence: true
+  # # , length: { minimum: 6}, format: { with: /\A[a-z0-9]+\z/i}
+  # validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name_kana, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :birth_date,presence:true
 end
