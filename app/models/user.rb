f49cadec033e23b33d529e8aa8954cc42b1613b7
songman78 <song.matsui@gmail.com>
2021-02-25 16:44:09 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, presence: true
-  # , length: { minimum: 6}, format: { with: /\A[a-z0-9]+\z/i}
+  , length: { minimum: 6}, format: { with: /\A[a-z0-9]+\z/i}
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
