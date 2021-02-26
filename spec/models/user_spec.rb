0e2babcf7f27df058c6198bdc322c050dc572df9
songman78 <song.matsui@gmail.com>
2021-02-26 18:08:25 +0900

    テストコードの追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,6 +72,46 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include  ("Password is too short (minimum is 6 characters)")
       end
+      it 'emailは＠を含まないと登録できないこと' do
+        @user.email = "@"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Email is invalid")  
+      end
+      it 'passwordは英語のみでは登録できないこと' do
+        @user.password = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password confirmation doesn't match Password")  
+      end
+      it 'passwordは数字のみでは登録できないこと' do
+        @user.password = "000000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password confirmation doesn't match Password")  
+      end
+      it 'passwordは全角では登録できないこと' do
+        @user.password = "アイウエオカ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Password confirmation doesn't match Password")  
+      end
+      it 'first_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
+        @user.first_name = "abcde"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("First name is invalid")  
+      end
+      it 'last_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
+        @user.last_name = "abcde"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Last name is invalid")  
+      end
+      it 'first_name_kanaは全角カタカナ以外では登録できないこと' do
+        @user.first_name_kana = "abecd"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("First name kana is invalid")  
+      end
+      it 'last_name_kanaは全角カタカナ以外では登録できないこと' do
+        @user.last_name_kana = "abcde"
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Last name kana is invalid")  
+      end
     end
   end
 end
