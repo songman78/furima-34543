8618df94093cc1db7a3a0e1166804dd517f34b7c
songman78 <song.matsui@gmail.com>
2021-02-26 18:08:10 +0900

    バリデーションに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
+    validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },on: :create
     validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
+    validates :last_name, format: { with: /[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /[ぁ-んァ-ン一-龥]/}
+    validates :last_name_kana,format: {with: /([ァ-ン]|ー)/}
+    validates :first_name_kana,format: {with: /([ァ-ン]|ー)/}
     validates :birth_date
   end
 end
