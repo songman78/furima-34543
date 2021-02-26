ca2f18eff4d53c71320d7b0e61b7ba5d3bc0e0c8
songman78 <song.matsui@gmail.com>
2021-02-26 20:22:16 +0900

    with_optionでまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   with_options presence: true do
     validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },on: :create
     validates :nickname
-    validates :last_name, format: { with: /[ぁ-んァ-ン一-龥]/}
-    validates :first_name, format: { with: /[ぁ-んァ-ン一-龥]/}
-    validates :last_name_kana,format: {with: /([ァ-ン]|ー)/}
-    validates :first_name_kana,format: {with: /([ァ-ン]|ー)/}
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+      validates :last_name 
+      validates :first_name
+    end
+    with_options format: { with: /\A([ァ-ン]|ー)+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end 
     validates :birth_date
   end
 end
