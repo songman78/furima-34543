2424e82ea32b195cdfc910ddb5015cc999290fac
songman78 <song.matsui@gmail.com>
2021-02-28 15:11:00 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,6 @@ class ItemsController < ApplicationController
 
 private
  def item_params
-  params.require(:item).permit(:user,:title,:description,:category_id,:condition_id,:delivery_change_id,:delivery_days_id,:prefecture_id,:price)
+  params.require(:item).permit(:user,:image,:title,:description,:category_id,:condition_id,:delivery_change_id,:delivery_days_id,:prefecture_id,:price).marge(user_id: current_user.id)
  end
 end
