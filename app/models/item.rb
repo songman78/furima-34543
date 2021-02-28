159012f7e2302fd6d5667ac20b6d94a8c0bddcab
songman78 <song.matsui@gmail.com>
2021-02-28 20:53:05 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options presence: true do
+    validates :user
+    validates :title
+    validates :text
+  with_options numericality: { other_than: 1 } 
+    validates :category_id
+    validates :condition_id 
+    validates :delivery_change_id
+    validates :delivery_days_id
+    validates :prefecture_id
+    validates :price
+  end
+  end
 end
