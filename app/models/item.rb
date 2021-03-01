87927701eef3279f3d9548fc77a8fae60b20cbf7
songman78 <song.matsui@gmail.com>
2021-03-01 19:36:51 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -10,14 +11,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :user
     validates :title, length: { maximum: 40 }
-    validates :text, length: { maximum: 1000 }
+    validates :description, length: { maximum: 1000 }
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id 
     validates :delivery_change_id
     validates :delivery_days_id
     validates :prefecture_id
-    validates :price
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
   end
 end
