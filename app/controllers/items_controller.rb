f34bfc31869b1cf1c993babb83c082049ce77ec7
songman78 <song.matsui@gmail.com>
2021-03-02 19:09:39 +0900

    item情報を@itemに代入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create]
 
   def index
-    # @items = Item.order('created_at DESC')
+    @items = Item.all
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -21,7 +22,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:user, :image, :title, :description, :category_id, :condition_id, :delivery_change_id,
-                                 :delivery_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:user, :image, :title, :description, :category_id, :condition_id, :delivery_change_id,:delivery_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
   end
 end
