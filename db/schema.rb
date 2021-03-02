5059840307add586380b8141f77dc70f46fa5dfd
songman78 <song.matsui@gmail.com>
2021-03-02 21:40:00 +0900

    コメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -49,12 +49,8 @@ ActiveRecord::Schema.define(version: 2021_03_02_085215) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -77,6 +73,4 @@ ActiveRecord::Schema.define(version: 2021_03_02_085215) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
-  add_foreign_key "orders", "items"
-  add_foreign_key "orders", "users"
 end
