71562657e857bdc3436f765c025103bd486c098d
songman78 <song.matsui@gmail.com>
2021-03-02 21:03:21 +0900

    ビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:user, :image, :title, :description, :category_id, :condition_id, :delivery_change_id,
-                                 :delivery_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:user, :image, :title, :description, :category_id, :condition_id, :delivery_change_id,:delivery_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
   end
 end
