7db226e263d334e6676995a92289d24ec08da51b
songman78 <song.matsui@gmail.com>
2021-03-03 17:52:45 +0900

    処理をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create,:edit, :update]
   before_action :set_item, only: [:edit, :show, :update]
+  before_action :check_current_user, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -22,9 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -43,5 +41,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def check_current_user
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
