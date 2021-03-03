c636155255d6dd75fb78fba99c45b2372158d894
songman78 <song.matsui@gmail.com>
2021-03-03 16:25:09 +0900

    ページ遷移の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -24,16 +24,28 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to root_path
+    elsif
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:user, :image, :title, :description, :category_id, :condition_id, :delivery_change_id,:delivery_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
