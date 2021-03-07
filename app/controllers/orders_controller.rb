fca7224cd78c8c68d5a3fd0e5a3fdc24baafb7d7
songman78 <song.matsui@gmail.com>
2021-03-07 12:01:57 +0900

    コントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:item, :user)
+  end
+end
