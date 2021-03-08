145ae077a22e676603a593afe96e01527ee7160c
songman78 <song.matsui@gmail.com>
2021-03-08 23:18:43 +0900

    attr_accessorにtoken追加、validation記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor  :zip_code1,:prefecture_id, :city, :address, :building, :phone_number,:user_id,:item_id,:order_id
+  attr_accessor  :zip_code1,:prefecture_id, :city, :address, :building, :phone_number,:user_id,:item_id,:order_id,:token
 
   with_options presence: true do
     validates :zip_code1, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -10,6 +10,7 @@ class OrderAddress
     validates :phone_number
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
