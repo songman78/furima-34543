097288016cf0248d5fb0c84b519f02874b7a91fc
songman78 <song.matsui@gmail.com>
2021-03-08 20:45:28 +0900

    validation記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :zip_code1, :prefecture_id, :city, :address, :building, :phone_number,:order,:user_id,:item_id
+  attr_accessor  :zip_code1,:prefecture_id, :city, :address, :building, :phone_number,:user_id,:item_id,:order_id
 
   with_options presence: true do
-    validates :zip_code1, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :zip_code1, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id, numericality: {other_than: 0}
     validates :city
     validates :address
-    validates :phone_number, format: {with: \A0[5789]0[-]?\d{4}[-]?\d{4}\z}
-    validates :user
-    validates :item
+    validates :phone_number
+    validates :user_id
+    validates :item_id
   end
 
   def save
     order = Order.create(user_id: user_id,item_id: item_id)
-    Address.create(zip_code1: zip_code1, prefecture_id: prefecture_id, city: city, address: address, building: building, order_id: order.id)
+    Address.create(zip_code1: zip_code1, prefecture_id: prefecture_id, city: city, address: address, building: building,phone_number: phone_number, order_id: order.id)
   end
 end
