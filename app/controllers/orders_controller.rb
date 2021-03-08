fbe8c273b766bdb3957573c4106997ede9e47055
songman78 <song.matsui@gmail.com>
2021-03-08 20:43:29 +0900

    コントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,28 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    # @order = Order.new
     @item = Item.find(params[:item_id])
-  end
-   
-  def new
     @order_address = OrderAddress.new
+  end
+  
+  def new
   end
 
   def create
-    @order_address = Order.new(order_params)
-    Address.create(address_params)
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      @order_address.save
+    @order_address.save
      redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:item_id, :user_id,:zip_code1, :prefecture_id, :city, :address, :building, :phone_number, :order)
-    # .merge(order_id: @order.id)
+    @item = Item.find(params[:item_id])
+    params.require(:order_address).permit(:zip_code1,:prefecture_id, :city, :address, :building, :phone_number, :order).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
