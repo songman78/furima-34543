ffca9283a8521d3f52ff256f43321262e9c6085f
songman78 <song.matsui@gmail.com>
2021-03-09 18:58:32 +0900

    バリデーション記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,7 +7,7 @@ class OrderAddress
     validates :prefecture_id, numericality: {other_than: 0}
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, format: {with: /\A^[0-9]+$\z/ },length: {maximum: 11}
     validates :user_id
     validates :item_id
     validates :token
